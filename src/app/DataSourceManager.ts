ed7eaa2032c14ba693d46c89e2067d81feb38f60
Martin Moreno <155martinmoreno@gmail.com>
2016-12-22 12:57:12 -0300

    Fixed: calling multiple data sources;

--- src/app/DataSourceManager.ts
+++ src/app/DataSourceManager.ts
@@ -2,28 +2,31 @@ import {DataSource} from "./DataSource";
 import {DataSourceCallback} from "./DataSourceCallback";
 
 export class DataSourceManager {
-  private dataSources: Array<DataSource>;
+    private dataSources: Array<DataSource>;
 
-  constructor(dataSource: DataSource, ...dataSources: DataSource[]) {
-    this.dataSources = [];
-    this.dataSources.push(dataSource);
-    dataSources.forEach(function (value: DataSource, index: number, array: DataSource[]) {
-      this.dataSources.push(value);
-    });
-  }
+    constructor(dataSource: DataSource, ...dataSources: DataSource[]) {
+        this.dataSources = [];
+        this.dataSources.push(dataSource);
 
-  public getData(key: string, callback: DataSourceCallback): any {
-    this.internal_get_data(0, key, callback);
-  }
+        if (!!dataSources) {
+            dataSources.forEach((value: DataSource, index: number, array: DataSource[]) => {
+                this.dataSources.push(value);
+            });
+        }
+    }
 
-  private internal_get_data(index: number, key: string, callback: DataSourceCallback): void {
-    this.dataSources[index].getData(key, function (data: any) {
-      if (!!data) {
-        callback.onData(data);
-      } else {
-        index++;
-        this.internal_get_data(index, key, callback);
-      }
-    });
-  }
+    public getData(key: string, callback: DataSourceCallback): any {
+        this.internal_get_data(0, key, callback);
+    }
+
+    private internal_get_data(index: number, key: string, callback: DataSourceCallback): void {
+        this.dataSources[index].getData(key, (data: any) => {
+            if (!!data) {
+                callback.onData(data);
+            } else {
+                index++;
+                this.internal_get_data(index, key, callback);
+            }
+        });
+    }
 }
--- src/test/data_source_manager-spec.ts
+++ src/test/data_source_manager-spec.ts
@@ -5,37 +5,66 @@ import {expect} from "chai";
 import {DataSourceCallback} from "../app/DataSourceCallback";
 
 class DummySource implements DataSource {
-  getData(key: string, callback: Function): any {
-    return "dummy_data";
-  }
+    getData(key: string, callback: Function): any {
+        return "dummy_data";
+    }
 
-  putData(key: string, value: any): void {
-  }
+    putData(key: string, value: any): void {
+    }
 }
 
 class DummyListener implements DataSourceCallback {
-  onData(data: any): void {
-  }
+    onData(data: any): void {
+    }
 }
 
 
 describe("DataSourceManager", function () {
-  it("Should get data from a Data Source", (done) => {
-    const key = "some_key";
-    const dummy_data = "dummy_data";
-    const dummySource = new DummySource();
-    const dummyListener = new DummyListener();
+    it("Should get data from a Data Source", (done) => {
+        const key = "some_key";
+        const dummy_data = "dummy_data";
+        const dummySource = new DummySource();
+        const dummyListener = new DummyListener();
 
-    sinon.stub(dummySource, "getData", function (key: string, callback: Function) {
-      return callback(dummy_data);
+        sinon.stub(dummySource, "getData", function (key: string, callback: Function) {
+            return callback(dummy_data);
+        });
+
+        sinon.stub(dummyListener, "onData", function (data: any) {
+            expect(data).to.equal(dummy_data);
+            done();
+        });
+
+        let manager: DataSourceManager = new DataSourceManager(dummySource);
+        manager.getData(key, dummyListener);
     });
 
-    sinon.stub(dummyListener, "onData", function (data: any) {
-      expect(data).to.equal(dummy_data);
-      done();
-    });
+    it("Should call 2 Sources and stop", function (done) {
+        const key = "some_key";
+        const dummy_data = "dummy_data";
+        const emptySource = new DummySource();
+        const dummySource = new DummySource();
+        const throwSource = new DummySource();
+        const dummyListener = new DummyListener();
 
-    let manager: DataSourceManager = new DataSourceManager(dummySource);
-    manager.getData(key, dummyListener);
-  });
+        sinon.stub(emptySource, "getData", function (key: string, callback: Function) {
+            return callback(null);
+        });
+
+        sinon.stub(dummySource, "getData", function (key: string, callback: Function) {
+            return callback(dummy_data);
+        });
+
+        sinon.stub(throwSource, "getData", function (key: string, callback: Function) {
+            throw new Error("Shouldn't be calling this");
+        });
+
+        sinon.stub(dummyListener, "onData", function (data: any) {
+            expect(data).to.equal(dummy_data);
+            done();
+        });
+
+        let manager: DataSourceManager = new DataSourceManager(emptySource, dummySource, throwSource);
+        manager.getData(key, dummyListener);
+    });
 });
